be05b642820a861e83eeb277298e53bd077c7b41
DESKTOP-VD3UNN1\fsalas <fsalas.y@gmail.com>
2018-08-04 13:42:22 -0500

    Update  : First Example

---
+++ Dev/AppEjemplo/AppEjemplo/AppEjemplo/PageExample.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppEjemplo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageExample : ContentPage
+	{
+        int sum = 0;
+
+		public PageExample ()
+		{
+			InitializeComponent ();
+		}
+        
+
+        private void btn1_Clicked(object sender, EventArgs e)
+        {
+            lb1.Text = "Hiciste click : " + sum++;
+
+            (sender as Button).Text = "Hiciste Clic!";
+        }
+    }
+}
