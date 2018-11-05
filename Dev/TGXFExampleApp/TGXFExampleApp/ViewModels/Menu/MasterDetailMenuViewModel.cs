4ef35672af02b8813ec7814c10547e3fe8743152
DESKTOP-0N6BQ0O\fsala <fsalas.y@gmail.com>
2018-11-04 20:52:28 -0600

    [Update] : Include custom control bindable properties

--- Dev/TGXFExampleApp/TGXFExampleApp/ViewModels/Menu/MasterDetailMenuViewModel.cs
+++ Dev/TGXFExampleApp/TGXFExampleApp/ViewModels/Menu/MasterDetailMenuViewModel.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using TGXFExampleApp.ViewModels.Shared;
 
 namespace TGXFExampleApp.ViewModels.Menu
 {
-    public class MasterDetailMenuViewModel
+    public class MasterDetailMenuViewModel : BaseViewModel
     {
+        private string _companyProp;
+        private string _usernameProp;
 
+        public string CompanyProp
+        {
+            get { return _companyProp; }
+            set { SetObservableProperty(ref _companyProp, value); }
+        }
+
+        public string UsernameProp
+        {
+            get { return _usernameProp; }
+            set { SetObservableProperty(ref _usernameProp, value); }
+        }
+
+        public MasterDetailMenuViewModel()
+        {
+            UsernameProp = "FSALAS";
+            CompanyProp = "Company123";
+        }
+
+        
     }
 }
